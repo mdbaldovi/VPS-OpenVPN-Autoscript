87ee7141b110e694a3ffc07ef86f01e9543940f0
mdbaldovi <60662458+mdbaldovi@users.noreply.github.com>
2020-02-05 15:01:12 +0800

    Add files via upload

---
+++ Components/about.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#About this script
+clear
+echo -e "VPS-OpenVPN-Autoscript" | lolcat
+echo -e "Original script by: Fornesia, Rzengineer, Clrkz, and Fawzya"
+echo -e "Modified by: Klai"
+echo -e "Forked by: mdbaldovi"
